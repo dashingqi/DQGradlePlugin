1f1928be353a75e5f5b74378a29b64d414ea306e
zhangqi61 <zhangqi61@baidu.com>
2021-04-18 23:12:25 +0800

    [update]

---
+++ buildsrc/src/main/groovy/MyPluginDemo.groovy
@@ -0,0 +1,9 @@
+class Api {
+    String url;
+    String name;
+}
+
+class MyPluginDemo {
+    Api baidu = new Api("http://www.baidu.com", "baidu")
+    Api tencent = new Api("http://www.tencent.com", "tencent")
+}
